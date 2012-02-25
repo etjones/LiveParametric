80948b3fbab34c59f116cdf7c932693076341152
Evan Jones <evan_t_jones@mac.com>
2012-02-25 08:45:15 -0800

    Adds a valid input textField to sliders.  Closes #10

--- ETJ/LiveParametric/VariableDicts/Slider.rb
+++ ETJ/LiveParametric/VariableDicts/Slider.rb
@@ -12,7 +12,7 @@ class Slider < VariableDict
         @showMinMax = showMinMax
         
         @html_width = 175
-        @html_height = 56
+        @html_height = 60
 
     end
     def val
@@ -34,6 +34,7 @@ class Slider < VariableDict
         title = varTitle
         sliderId = unique_id+"_slider"
 
+        textW = Math::log10( maxVal).ceil + ( minVal < 0 ? 1 : 0) + 1
         if integerOnly
             cur = curVal.to_i
             min = minVal.to_i
@@ -46,19 +47,25 @@ class Slider < VariableDict
             max = sprintf("%.2f",maxVal)            
             rValStr = 'Number(this.value).toFixed(2)'
             stepStr = "step='#{(maxVal-minVal)/20.0}'"
+            textW += 3
         end
         minMaxStr = ''
         if showMinMax
             minMaxStr = %Q{\n        <tr><td align ="left">#{min}</td><td align="right">#{max}</td></tr>\n}
         end
-        
-        onChangeStr = %Q{onChange="document.getElementById('#{unique_id}').innerHTML = #{rValStr}; did_change('#{unique_id}', #{rValStr})"}
-   
+        dgb = "document.getElementById"
+        sliderChangeStr = %Q{onChange="#{dgb}('#{unique_id}').value = #{rValStr}; did_change('#{unique_id}', #{rValStr})"}
+        textChangeStr   = %Q{onChange="#{dgb}('#{sliderId}' ).value = #{rValStr}; did_change('#{unique_id}', #{rValStr})"}
+    
         sliderStr = <<-EOS
         <table border="0" width=#{sliderWidth}px align="center">
-        <tr><td align ="left">#{title}:</td><td align="right" id="#{unique_id}">#{cur}</td></tr>
-        <tr><td colspan="2">
-            <input type="range" id="#{sliderId}" value="#{cur}" min="#{min}" max="#{max}" #{stepStr} #{onChangeStr}/>
+        <tr><td align ="left">#{title}:</td>
+            <td align="right"> 
+                <input type="text" id="#{unique_id}" size="#{textW}" value="#{cur}" #{textChangeStr}">
+            </td>        
+        </tr>
+        <tr><td colspan="2" align="center">
+            <input type="range" id="#{sliderId}" value="#{cur}" min="#{min}" max="#{max}" #{stepStr} #{sliderChangeStr}/>
         </td></tr>#{minMaxStr}
         </table>
 
